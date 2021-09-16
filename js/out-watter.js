433d6a73dee30a8597218b98de59ece0550c30dc
Jesus Rodriguez <jrodriguez@loatech.co>
2021-09-15 22:39:49 -0500

    JS para quitar publicidad

---
+++ js/out-watter.js
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+		
+		document.addEventListener("DOMContentLoaded", () => {
+
+			let allDivs = document.querySelectorAll("div")
+
+			allDivs[allDivs.length-1].innerHTML = ""
+	
+		}) 
+		
+	</script>
